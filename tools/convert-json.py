ed647b5aee295d427e4dd88e70b7546bdf8990e7
Adrian <github@netzkind.org>
2016-07-15 15:17:06 +0200

    filter stations

--- tools/convert-json.py
+++ tools/convert-json.py
@@ -1,9 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import json
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 aws = {}
 aws['RadioStation'] = [];
 
-with open ('../app/radio/assets/stations.json','r') as f:
+search_terms = ['ndr', 'swr', 'wdr', 'deutschlandradio', 'bayern', 'antenne', 'hamburg', 'hessen', 'niedersachen']
+
+with open ('./stations.json','r') as f:
     data = json.load(f)
+    #print data
     for row in data['radio-station']:
-        print (row)
+        for term in search_terms:
+            if term in row['title'].lower() or term in row['description'].lower(): 
+                print (row['title'].encode('utf-8'))
